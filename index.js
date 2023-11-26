e3c012c3fccc2a8a009ec38daff40b0043d33023
ziaro <zia.ro@live.com>
2023-11-25 23:15:17 -0500

    third

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ leftHead.textContent = 'ProTraders';
 const rightHead = document.getElementById('main-child1-div7');
 rightHead.textContent = 'Login/Logout';
 
-
+//================================================================= inside page nav buttons
 const mainChildThirdEl = document.getElementById('main-dv-child3');
 //mainChildThirdEl.appendChild(document.createElement('div')).setAttribute('id', 'container1')
 elemGenerator(mainChildThirdEl, 3, 'div', 'id', 'container');
@@ -35,41 +35,70 @@ elemGenerator(document.getElementById('container0'), 4, 'button', 'id', 'nav-btn
 
 let navBtn1 = document.getElementById('nav-btn0')
 navBtn1.textContent = 'Chart'
+navBtn1.onclick = function (){
+  let cont1 = document.getElementById('container1')
+  cont1.style.display = 'block'
+  let cont2 = document.getElementById('container2')
+  cont2.style.display = 'block'
+  let cont3 = document.getElementById('portf-dv')
+  cont3.style.display = 'none'
+}
 let navBtn2 = document.getElementById('nav-btn1')
 navBtn2.textContent = 'Portfolio'
+navBtn2.onclick = function (){
+  let cont1 = document.getElementById('container1')
+  cont1.style.display = 'none'
+  let cont2 = document.getElementById('container2')
+  cont2.style.display = 'none'
+  let portfolio = document.getElementById('main-dv-child3')
+    portfolio.appendChild(document.createElement('div')).setAttribute('id', 'portf-dv')
+}
 let navBtn3 = document.getElementById('nav-btn2')
 navBtn3.textContent = 'Chart1'
 let navBtn4 = document.getElementById('nav-btn3')
 navBtn4.textContent = 'Chart2'
+let insideNav = document.getElementById('container0')
+for (let item of insideNav.children){
+  item.setAttribute('class', 'ins-nav-btn')
+}
+//====================================================== transaction form  > order > sell
+elemGenerator(document.getElementById('container2'), 1, 'form', 'id', 'transaction-frm')
+let transFrm = document.getElementById('transaction-frm0');
 
-//elemGenerator(document.getElementById('container1'), 2, 'button', 'id', 'test')
-// mainChildThirdEl.childNodes.forEach(function(item) {
-//   elementId++;
-//   item.setAttribute('id', `${item.getAttribute('class') + elementId}`)
+transFrm.appendChild(document.createElement('label')).setAttribute('id', 'trans-frm-lbl1')
+let lableOne = document.getElementById('trans-frm-lbl1')
+lableOne.textContent = "Symbol: "
+transFrm.appendChild(document.createElement('input')).setAttribute('id', 'trans-frm-inp1')
+let inputOne = document.getElementById('trans-frm-inp1')
+inputOne.type = 'text'
 
-// })
+transFrm.appendChild(document.createElement('label')).setAttribute('id', 'trans-frm-lbl2')
+let lableTwo = document.getElementById('trans-frm-lbl2')
+lableTwo.textContent = "Price: "
+transFrm.appendChild(document.createElement('input')).setAttribute('id', 'trans-frm-inp2')
+let inputTwo = document.getElementById('trans-frm-inp2')
+inputTwo.type = 'text'
 
-elemGenerator(document.getElementById('container2'), 2, 'button', 'id', 'transaction-btn')
-let buyBtn = document.getElementById('transaction-btn0');
-buyBtn.textContent = 'Buy'
-let sellBtn = document.getElementById('transaction-btn1')
-sellBtn.textContent = 'Sell'
-//============================
-const isElementLoaded = async selector => {
-  while ( document.querySelector(selector) === null) {
-    await new Promise( resolve => requestAnimationFrame(resolve) )
-  }
-  return document.querySelector(selector);
-};
+transFrm.appendChild(document.createElement('label')).setAttribute('id', 'trans-frm-lbl3')
+let lableThree = document.getElementById('trans-frm-lbl3')
+lableThree.textContent = "Quantity: "
+transFrm.appendChild(document.createElement('input')).setAttribute('id', 'trans-frm-inp3')
+let inputThree = document.getElementById('trans-frm-inp3')
+inputThree.type = 'text'
 
-// I'm checking for a specific class .file-item and then running code. You can also check for an id or an element.
-isElementLoaded('#container').then((selector) => {
-  let cloneChart = document.getElementById('container').cloneNode(true);
-  document.getElementById('container1').appendChild(cloneChart);
-});
+transFrm.appendChild(document.createElement('button')).setAttribute('id', 'trans-frm-btn1')
+let btnOne = document.getElementById('trans-frm-btn1')
+btnOne.textContent = 'Buy'
+btnOne.type = 'submit'
+
+transFrm.appendChild(document.createElement('button')).setAttribute('id', 'trans-frm-btn2')
+let btnTwo = document.getElementById('trans-frm-btn2')
+btnTwo.textContent = 'Sell'
+btnTwo.type = 'submit'
+
 //=================================
 // presenting the chart
-window.onload = function () {
+let p = window.onload = function () {
   var dps1 = [], dps2= [];
   var stockChart = new CanvasJS.StockChart("container1",{
     title:{
@@ -147,17 +176,120 @@ window.onload = function () {
     return result;
   }
 }
-// Fetching open and close price
-// async function getData(){
-//   const res = await fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=OFq3eK3caAqNsWB33pQfQwVtTygClgup')
-//   const data = await res.json()
-//   console.log('I am running')
-//   console.log(data)
-// }
-// getData();
-// create form page buttons
+
+//========================================= Represent chart based on search input
+function searchTickerFu(tick) {
+  var dps1 = [], dps2= [];
+  var stockChart = new CanvasJS.StockChart("container1",{
+    title:{
+      text:"Stock Charty"
+    },
+    subtitles: [{
+      text:"Simple Moving Average"
+    }],
+    charts: [{
+      axisY: {
+        prefix: "$"
+      },
+      legend: {
+        verticalAlign: "top",
+        cursor: "pointer",
+        itemclick: function (e) {
+          if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+          e.chart.render();
+        }
+      },
+      toolTip: {
+        shared: true
+      },
+      data: [{
+        type: "candlestick",
+        showInLegend: true,
+        name: "Stock Price",
+        yValueFormatString: "$#,###.00",
+        xValueType: "dateTime",
+        dataPoints : dps1
+      }],
+    }],
+    navigator: {
+      data: [{
+        dataPoints: dps2
+      }],
+      slider: {
+        minimum: new Date(2018, 03, 01),
+        maximum: new Date(2018, 05, 01)
+      }
+    }
+  });
+  $.getJSON(`https://canvasjs.com/data/docs/${tick}usd2018.json`, function(data) {
+    for(var i = 0; i < data.length; i++){
+      dps1.push({x: new Date(data[i].date), y: [Number(data[i].open), Number(data[i].high), Number(data[i].low), Number(data[i].close)]});
+      dps2.push({x: new Date(data[i].date), y: Number(data[i].close)});
+    }
+    stockChart.render();
+    var sma = calculateSMA(dps1, 7);
+    stockChart.charts[0].addTo("data", { type: "line", dataPoints: sma, showInLegend: true, yValueFormatString: "$#,###.00", name: "Simple Moving Average"})
+  });
+  function calculateSMA(dps, count){
+    var avg = function(dps){
+      var sum = 0, count = 0, val;
+      for (var i = 0; i < dps.length; i++) {
+        val = dps[i].y[3]; sum += val; count++;
+      }
+      return sum / count;
+    };
+    var result = [], val;
+    count = count || 5;
+    for (var i=0; i < count; i++)
+      result.push({ x: dps[i].x , y: null});
+    for (var i=count - 1, len=dps.length; i < len; i++){
+      val = avg(dps.slice(i - count + 1, i));
+      if (isNaN(val))
+        result.push({ x: dps[i].x, y: null});
+      else
+        result.push({ x: dps[i].x, y: val});
+    }
+    return result;
+  }
+}
+//=========================================
 
 
+//====================================================================== Child 2, search content
+elemGenerator(document.getElementById('main-dv-child2'),2, 'div', 'id', 'main-ch2-dv')
+console.log(document.getElementById('main-dv-child2').children)
+for (let item of document.getElementById('main-dv-child2').children){
+  item.setAttribute('class', 'mn-dv2-child')
+}
+// Creating search input
+let ticker = 'BTC'
+document.getElementById('main-ch2-dv0').textContent = ticker
+document.getElementById('main-ch2-dv1').appendChild(document.createElement('form')).setAttribute('id', 'search-frm')
+let searchFrm = document.getElementById('search-frm')
+
+searchFrm.appendChild(document.createElement('label')).textContent = 'ticker'
+searchFrm.appendChild(document.createElement('input')).setAttribute('id', 'search-input')
+let searchFrmInput = document.getElementById('search-input')
+  searchFrmInput.type = 'text'
+searchFrmInput.placeholder = 'BTC'
+searchFrmInput.addEventListener('keydown', function (e){
+
+  console.log("Search Pressed!")
+  if (e.key === 'Enter'){
+    e.preventDefault()
+    if (searchFrmInput.value !== ""){
+      searchTickerFu(searchFrmInput.value)
+      searchFrmInput.value = ''
+    }
+  }
+})
+
+let test = document.getElementById('main-dv-child5')
+console.log(test)
 // creates child elements
 function elemGenerator(parEl, quanEl, typeEl, attrName, attrVal) {
   for (let i = 0; i < quanEl; i++) {
@@ -165,3 +297,5 @@ function elemGenerator(parEl, quanEl, typeEl, attrName, attrVal) {
     console.log('hi')
   }
 }
+
+
